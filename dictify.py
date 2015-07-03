d067107e104728e88835f41645db6b5b9894fbaa
Thierry Delamare <t.delamare@epiconcept.fr>
2015-07-03 16:38:57 +0200

    dictify can handle simple list too and more accurate documentation

--- dictify.py
+++ dictify.py
@@ -1,19 +1,38 @@
-# make a dict of dict of dict from a result list of dict of dict
-# find item to use as key in n.item
 # useful to access results of register
 #
+# make a dict of of dict from a result list of dict
+#
+# - { set_fact: { lst: [ foo, bar ] }}
 # - { command: tst "{{ item }}", register: tsts, with_items: lst }
-# - { set_facts: { tsts_d: "{{ tsts.results|dictify }}" }
+# - { set_fact: { tsts_d: "{{ tsts.results|dictify }}" }
 # - { command: cmd, with_items: lst, when: tsts_d[item].rc == 0 }
-
 # 
-# results: [ *: *, item: { name: foo, ip: 1.2.3.4 }, ... ]
-#       -> { foo: *: *, item: { name: foo, ip: 1.2.3.4 }, ... }
+# tsts.results: [ { item: foo, rc: 0, stdout: OK, ... },      { item: bar, rc: 1, stdout: KO, ... } ]
+# tsts_d: { foo:  { item: foo, rc: 0, stdout: OK, ... }, bar: { item: foo, rc: 1, stdout: KO, ... } }
+#
+# or
+#
+# make a dict of dict of dict from a result list of dict of dict
+#
+# find item to use as key in n.item
+#
+# - { set_fact: { lst: [ { name: foo, attr: some }, { name: bar, attr: other } ] }}
+# - { command: tst {{ item.name }} {{ item.attr }}, register: tsts, with_items: lst }
+# - { set_fact: { tsts_d: "{{ tsts.results|dictify }}" }
+# - { command: cmd, with_items: lst, when: tsts_d[item.name].rc == 0 }
+#
+# tsts.results: [ { item: { name: foo, attr: some  }, rc: 0, stdout: OK, ... },
+#                 { item: { name: bar, attr: other }, rc: 1, stdout: KO, ... } ]
+# tsts_d: { foo:  { item: { name: foo, attr: some  }, rc: 0, stdout: OK, ... },
+#           bar:  { item: { name: bar, attr: other }, rc: 1, stdout: KO, ... },
 
 def dictify(l, n='name'):
     r = {}
     for i in l:
-        r[i['item'][n]] = i
+        if type(i['item']) == type(dict()):
+            r[i['item'][n]] = i
+        else:
+            r[i['item']] = i
 
     return r
 
